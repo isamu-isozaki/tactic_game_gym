Fixed minor masking bug

tactic_game_gym/tactic_game/env_classes/game_utility.py
@@ -128,7 +128,7 @@ class Attack(Mobilize):
         #X[i], Y[i] is the x and y coordinates of all of the players when the ith player is at (0,0)
         XY = np.concatenate([X[..., None], Y[..., None]], axis = 2)
         attack_range_mags = mags / (self.attrange_div_const*self.max_speed)
-        attack_range_mags[is_archer[living]] = 1/self.attrange_div_const - attack_range_mags[is_archer]#The higher the velocity, the lower the range
+        attack_range_mags[is_archer[living]] = 1/self.attrange_div_const - attack_range_mags[is_archer[living]]#The higher the velocity, the lower the range
         attack_range_mags[is_wall[living]] = 0
         base_index = 12
         attack_range = self.range_factor*np.einsum("i,i->i",attack_range_mags, self.board_sight[living, base_index])[:, None]#set max value of einsum to 1